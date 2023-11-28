c8fa924bf575f5c2d57951f049d6e48799306bd4
Guillermo Sanchez <lsanchezp5@upao.edu.pe>
2023-11-27 23:14:37 -0500

    init project

---
+++ src/main/java/com/iread/backend/project/IreadProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.iread.backend.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IreadProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IreadProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iread/backend/project/IreadProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iread.backend.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IreadProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
